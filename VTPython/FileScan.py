919b4f3858791ac9855000d3644da38f099f83cf
Nilay Sangani <sanganinilay@hotmail.com>
2015-05-14 20:37:15 +0400

    Revert "VirusTotalASP.NETPythonv_1.0"
    This reverts commit 8d7cb861eaab2d96e9ef297faf19bee86c9874fb.

--- VTPython/FileScan.py
+++
@@ -1,9 +0,0 @@
-import sys
-import postfile
-host = "www.virustotal.com"
-selector = "https://www.virustotal.com/vtapi/v2/file/scan"
-fields = [("apikey", "Enter Your API Key")]
-file_to_send = open(sys.argv[1], "rb").read()
-files = [("file",sys.argv[1],file_to_send)]
-json = postfile.post_multipart(host, selector, fields, files)
-print json
